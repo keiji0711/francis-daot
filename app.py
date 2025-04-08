f942206ae45e150621fdf27ec097ed8aaf13f2fe
keiji0711 <dexterjamesdegayo07@gmail.com>
2025-04-09 00:49:18 +0800

    updated app.py

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from flask import Flask
 from routes.main import bp as main_bp
 from routes.staff import bp as staff_bp
 from routes.auth import bp as auth_bp
+from routes.deletee_routes import bp as deletee_bp
+from routes.adding_routes import bp as adding_bp
+from routes.update_routes import bp as update_bp
 
 app = Flask(__name__)
 
@@ -11,6 +14,9 @@ app.secret_key = "12345"
 app.register_blueprint(main_bp)
 app.register_blueprint(staff_bp)
 app.register_blueprint(auth_bp)
+app.register_blueprint(deletee_bp)
+app.register_blueprint(adding_bp)
+app.register_blueprint(update_bp)
 
 if __name__ == '__main__':
     app.run(debug=True)
